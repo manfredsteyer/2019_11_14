93b2d7db2be4544c6a3143f3842d2f127d6eb32f
Manfred Steyer <manfred.steyer@gmx.net>
2019-11-13 18:18:37 +0100

    meta

--- projects/flight-app/src/app/+state/index.ts
+++ projects/flight-app/src/app/+state/index.ts
@@ -16,4 +16,13 @@ export const reducers: ActionReducerMap<State> = {
 };
 
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export const debug = (reducer: ActionReducer<any>) => {
+  
+  return (state, action) => {
+    console.debug('state', state);
+    console.debug('action', action);
+    return reducer(state, action)
+  }
+}
+
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [debug] : [];
